3466caef0afc9c56e937a7dcf6986e5190a44047
Donna Torres <todonnatorres@icloud.com>
2020-06-06 04:39:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,15 +10,6 @@ def happy_birthday(birthday_kids)
   end
 
 
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    if age < 13
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
-    else
-     age > 12
-      puts "You are too old for this."
-   end 
-    
-  end
+
   
 
